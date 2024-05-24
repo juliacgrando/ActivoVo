19081074740813de74c6c2fea4e288092164113c
Juliacgrando <114629830+juliacgrando@users.noreply.github.com>
2024-05-24 09:14:00 -0300

    HACKATRUCK
    Hackatruck ActivoVo

---
+++ RemindersView.swift
@@ -0,0 +1,18 @@
+//
+//  RemindersView.swift
+//  ActivoVo
+//
+//  Created by Turma01-23 on 23/05/24.
+//
+
+import SwiftUI
+
+struct RemindersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemindersView()
+}
